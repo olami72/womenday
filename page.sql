7270ef4ec8255180990e8a83d5edb6bdc3851f8d
SULTAN ERINFOLAMI <olami5003@gmail.com>
2024-03-08 03:23:09 -0800

    web page

---
+++ page.sql
@@ -0,0 +1,5 @@
+What is a short note about a web page
+A web page (or webpage) is a document on the World Wide Web "with its own address". 
+Web pages are delivered by a web server to the user and displayed in a web browser to act as a "retrieval unit" 
+for the information stored within it.
+ 
